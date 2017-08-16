38a3609ba777398588037b0cc2507bdc0c6e4bda
Rahul Bhatia <rbhatia46@gmail.com>
2017-08-16 20:07:32 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpModule } from '@angular/http';
+import { IonicStorageModule } from '@ionic/storage'; 
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import {SettingsPage} from '../pages/settings/settings';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { WeatherProvider } from '../providers/weather/weather';
 
 @NgModule({
   declarations: [
@@ -17,11 +21,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    SettingsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule,
+    IonicStorageModule.forRoot()  
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +36,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    SettingsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    WeatherProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
+import {Storage} from '@ionic/storage';  
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  weather:any;
+  location:{
+    city:string,
+    state:string
+  }
+  constructor(
+    public navCtrl: NavController, 
+    private weatherProvider:WeatherProvider,
+    private storage:Storage) { // To inject as a dependency you need to pass this in the constructor
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  ionViewWillEnter(){ // Similar to ngOnInit()
+      this.storage.get('location').then((val) => {
+        if(val!=null){
+          this.location = JSON.parse(val);
+        }else{
+            this.location={
+      city : 'Indore',
+      state: 'India'
+    }
+        }
+
+this.weatherProvider.getWeather(this.location.city,this.location.state).subscribe(weather => {
+      //console.log(weather);  JUST FOR TESTING PURPOSE
+      this.weather = weather.current_observation;
+    });
+
+      }); 
   }
 
 }
